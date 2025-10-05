4659193bb79efc59e5f4f5e1b94f16e2af795e7d
ksmushkov-cmd <ksmushkov@bk.ru>
2025-10-05 19:08:06 +0700

    Update lab02исправ.cpp

--- lab02исправ.cpp
+++ lab02исправ.cpp
@@ -12,41 +12,39 @@ using namespace std;
 int main() {  
 
   // P обозначем как уравнение состояния углекислого газа
-  double P;
-  
-  double a;
+  double a, b, P;
+ 
   cout << " a = ";
   cin >> a;
 
-  double b;
   cout << " b = ";
   cin >> b;
   
   // температура газа
-  double GasTemperature;
-  cout << " GasTemperature = ";
-  cin >> GasTemperature;
+  double gasTemperature;
+  cout << " gas Temperature = ";
+  cin >> gasTemperature;
   
   // объем газа
-  double GasVolume;
-  cout << " GasVolume = ";
-  cin >> GasVolume;
+  double gasVolume;
+  cout << " gas Volume = ";
+  cin >> gasVolume;
   
   // Пока объем газа меньше 0.2 м^3.
-  while (GasVolume < 0.2) {
-    P = (0.0635 * GasTemperature) / (GasVolume - b) - a / (GasVolume * GasVolume);
+  while (gasVolume < 0.2) {
+    P = (0.0635 * gasTemperature) / (gasVolume - b) - a / pow(gasVolume, 2);
     cout << " P = " << P << endl;
     // 0.05 - шаг объема газа до 0.2.
-    GasVolume += 0.05;
+    gasVolume += 0.05;
   }
   
   do {
-    P = (0.0635 * GasTemperature) / (GasVolume - b) - a / (GasVolume * GasVolume);
+    P = (0.0635 * gasTemperature) / (gasVolume - b) - a / pow(gasVolume, 2);
     cout << " P = " << P << endl;
     // 0.2 - шаг объема газа после 0.2.
-    GasVolume += 0.2;
+    gasVolume += 0.2;
     // Пока объем газа меньше или равно 1.
-  } while(GasVolume <= 1.0);
+  } while (gasVolume <= 1.0);
        
   return 0;
 }
